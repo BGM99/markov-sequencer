a33606fd2568fd79f60e7a4306fd82ee69314ffc
BGM99 <45606744+BGM99@users.noreply.github.com>
2025-01-20 00:35:56 +0100

    Add generateSingleNoteCHain

--- Source/Core/Midi/Models/MarkovModel.cpp
+++ Source/Core/Midi/Models/MarkovModel.cpp
@@ -14,23 +14,83 @@ MarkovModel::~MarkovModel()
 
 void MarkovModel::generateFromSequence(Array<Note> sortedSelection)
 {
+    if (true)
+    {
+        generateSingleNoteChain(sortedSelection);
+    } else
+    {
+        generateMultiNoteChain(sortedSelection);
+    }
+
+    this->buildMatrix();
+    this->buildInitialVector();
+}
+
+void MarkovModel::generateSingleNoteChain(const Array<Note>& sortedSelection)
+{
+    float lastBeat = sortedSelection.getReference(sortedSelection.size() - 1).getBeat();
+
     for (int i = 0; i < sortedSelection.size() - 1; ++i)
     {
-        Note prev = sortedSelection.getReference(i);
-        Note next = sortedSelection.getReference(i + 1);
+        Note left = sortedSelection.getReference(i);
+        Note right = sortedSelection.getReference(i + 1);
+
+        int offset = 0;
+
+        if (left.getBeat() == lastBeat)
+        {
+            this->States.insert(left);
+            this->SoundFrequency[left] += 1;
+            break;
+        }
+
+        while (left.getBeat() == right.getBeat())
+        {
+            offset++;
+            right = sortedSelection.getReference(i + offset + 1);
+        }
+
+        i += offset;
+
+        this->TransitionFrequency[{left, right}] += 1;
+        this->SoundFrequency[left] += 1;
+        this->States.insert(left);
+    }
+}
+
+void MarkovModel::generateMultiNoteChain(const Array<Note>& sortedSelection)
+{
+    for (int i = 0; i < sortedSelection.size() - 1; ++i)
+    {
+        Note startNote = sortedSelection.getReference(i);
+        std::vector<Note> prev = getAllNotesFromBeat(sortedSelection, startNote.getBeat());
+        std::vector<Note> next = getAllNotesFromBeat(sortedSelection, startNote.getBeat() + 1);
 
         this->TransitionFrequency[{prev, next}] += 1;
 
         this->SoundFrequency[prev] += 1;
 
-        this->states.insert(prev);
+        this->States.insert(prev);
     }
 
-    this->states.insert(sortedSelection.getReference(sortedSelection.size() - 1));
-    this->SoundFrequency[sortedSelection.getReference(sortedSelection.size() - 1)] += 1;
+    this->States.insert(getAllNotesFromBeat(sortedSelection, sortedSelection.size() - 1));
+}
 
-    this->buildMatrix();
-    this->buildInitialVector();
+std::vector<Note> MarkovModel::getAllNotesFromBeat(const Array<Note>& sortedSelection, int index)
+{
+    std::vector<Note> chord;
+    Note prev = sortedSelection.getReference(index);
+    Note next = sortedSelection.getReference(index + 1);
+
+    chord.push_back(prev);
+
+    for (int b = index + 2; prev.getBeat() == next.getBeat(); ++b)
+    {
+        chord.push_back(next);
+        next = sortedSelection.getReference(b);
+    }
+
+    return chord;
 }
 
 void MarkovModel::buildMatrix()
@@ -39,13 +99,13 @@ void MarkovModel::buildMatrix()
 
     for (int r = 0; r < Size(); ++r)
     {
-        auto itRow = this->states.begin();
+        auto itRow = this->States.begin();
         std::advance(itRow, r);
 
         int sum = 0;
         for (int c = 0; c < this->Size(); ++c)
         {
-            auto itCol = this->states.begin();
+            auto itCol = this->States.begin();
             std::advance(itCol, c);
 
             auto row = *itRow;
@@ -62,7 +122,7 @@ void MarkovModel::buildMatrix()
         float prob[this->Size()];
         for (int c = 0; c < this->Size(); ++c)
         {
-            auto itCol = this->states.begin();
+            auto itCol = this->States.begin();
             std::advance(itCol, c);
 
             (*this->StateMatrix)(r,c) = float(this->TransitionFrequency[{*itRow, *itCol}] / sum);
