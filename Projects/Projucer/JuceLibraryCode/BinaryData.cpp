c8ca8ff96b7a80075ad33ea81961cb16c8b8dfb5
BGM99 <45606744+BGM99@users.noreply.github.com>
2025-02-07 16:19:35 +0100

    Another Try to fix Build

--- Projects/Projucer/JuceLibraryCode/BinaryData.cpp
+++ Projects/Projucer/JuceLibraryCode/BinaryData.cpp
@@ -4,6 +4,8 @@
 
 */
 
+#include <cstring>
+
 namespace BinaryData
 {
 
@@ -4664,6 +4666,7 @@ static const unsigned char temp_binary_data_117[] =
 const char* translations_json = (const char*) temp_binary_data_117;
 
 
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -5043,13 +5046,12 @@ const char* originalFilenames[] =
     "translations.json"
 };
 
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
-    {
-        if (namedResourceList[i] == resourceNameUTF8)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
             return originalFilenames[i];
-    }
 
     return nullptr;
 }
